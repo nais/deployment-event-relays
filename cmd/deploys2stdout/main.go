0feef47858db16e257def3e37024902912e4e5bc
Kim Tore Jensen <kimtjen@gmail.com>
2019-07-18 14:26:35 +0200

    Parse and use Kafka command-line flags

--- cmd/deploys2stdout/main.go
+++ cmd/deploys2stdout/main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	"github.com/navikt/deployment-event-relays/pkg/kafka/config"
+	flag "github.com/spf13/pflag"
+)
 
+func main() {
+	kafkaConfig := config.DefaultConsumer()
+	config.SetupFlags(&kafkaConfig)
+
+	flag.Parse()
 }
